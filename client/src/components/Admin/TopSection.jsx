1b18bb52b46ee545440da93d63b7b1115180a9bd
Gohil Harsh <66783334+harsh1110@users.noreply.github.com>
2022-02-07 22:59:41 +0530

    some more changes

--- client/src/components/Admin/TopSection.jsx
+++ client/src/components/Admin/TopSection.jsx
@@ -24,28 +24,28 @@ const TopSection = () => {
         <h2 className="text-start title">Hii, {user.name}</h2>
         <Grid container spacing={{ xs: 4, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             <Grid item xs={12} md={3}>
-                <div style={{ borderRadius: "35px" }} className="card blue text-center py-2">
+                <div style={{ borderRadius: "15px" }} className="card blue text-center py-2">
                     <h4 className='blue-icon text-center my-4'><AccountCircleOutlinedIcon /></h4>
                     <h3 className='number mt-2'>100</h3>
                     <p className='sub-title mb-4'>Total Users</p>
                 </div>
             </Grid>
             <Grid item xs={12} md={3}>
-                <div style={{ borderRadius: "35px" }} className="card yellow text-center py-2">
+                <div style={{ borderRadius: "15px" }} className="card yellow text-center py-2">
                     <h4 className='yellow-icon text-center my-4'><AccessTimeIcon /></h4>
                     <h3 className='number mt-2'>150</h3>
                     <p className='sub-title mb-4'>Pennding Applications</p>
                 </div>
             </Grid>
             <Grid item xs={12} md={3}>
-                <div style={{ borderRadius: "35px" }} className="card green text-center py-2">
+                <div style={{ borderRadius: "15px" }} className="card green text-center py-2">
                     <h4 className='green-icon text-center my-4'><CheckCircleOutlineOutlinedIcon /></h4>
                     <h3 className='number mt-2'>50</h3>
                     <p className='sub-title mb-4'>Approved Applications</p>
                 </div>
             </Grid>
             <Grid item xs={12} md={3}>
-                <div style={{ borderRadius: "35px" }} className="card red text-center py-2">
+                <div style={{ borderRadius: "15px" }} className="card red text-center py-2">
                     <h4 className='red-icon text-center my-4'><HighlightOffOutlinedIcon /></h4>
                     <h3 className='number mt-2'>10</h3>
                     <p className='sub-title mb-4'>Rejected Application</p>
